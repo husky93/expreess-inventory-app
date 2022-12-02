d7b8ac2a03949c6e376d4031bea9717746db2ab5
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-02 14:16:41 +0100

    Setup categories controller template

---
+++ controllers/categoriesController.js
@@ -0,0 +1,35 @@
+exports.index = (req, res) => {
+  res.send('NOT IMPLEMENTED: index GET');
+};
+
+exports.category_detail = (req, res) => {
+  res.send('NOT IMPLEMENTED: Category Detail GET');
+};
+
+exports.category_detail = (req, res) => {
+  res.send('NOT IMPLEMENTED: Category Detail GET');
+};
+
+exports.category_delete_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Category Delete GET');
+};
+
+exports.category_delete_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Category Detail POST');
+};
+
+exports.category_create_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Category Create GET');
+};
+
+exports.category_create_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Category Create POST');
+};
+
+exports.category_update_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Category Update GET');
+};
+
+exports.category_update_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Category Update POST');
+};
