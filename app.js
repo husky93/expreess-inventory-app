37d4a6d14a66a95fe8b86cc1543da00ecba024a9
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-02 14:17:06 +0100

    Setup categories router

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const categoriesRouter = require('./routes/categories');
 
 dotenv.config();
 
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/categories', categoriesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/categories.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const categories_controller = require('../controllers/authorController');
+
+/* GET users listing. */
+router.get('/', categories_controller.index);
+
+router.get('/:id', categories_controller.category_detail);
+
+router.get('/:id/delete', categories_controller.category_delete_get);
+
+router.post('/:id/delete', categories_controller.category_delete_post);
+
+router.get('/create', categories_controller.category_create_get);
+
+router.post('/create', categories_controller.category_create_post);
+
+router.get('/:id/update', categories_controller.category_update_get);
+
+router.post('/:id/update', categories_controller.category_update_post);
+
+module.exports = router;
