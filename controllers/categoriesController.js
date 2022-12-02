9fe26fca6d16fce5a74fcc1c4a832a385078f30a
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-02 15:32:45 +0100

    Display each category details with all items that the category contains

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,8 +1,10 @@
 const Category = require('../models/category');
+const Item = require('../models/item');
+const async = require('async');
 
 exports.index = (req, res, next) => {
   Category.find()
-    .sort([['name', 'ascending']])
+    .sort([['title', 'ascending']])
     .exec((err, categories_list) => {
       if (err) {
         return next(err);
@@ -14,8 +16,32 @@ exports.index = (req, res, next) => {
     });
 };
 
-exports.category_detail = (req, res) => {
-  res.send('NOT IMPLEMENTED: Category Detail GET');
+exports.category_detail = (req, res, next) => {
+  async.parallel(
+    {
+      category(callback) {
+        Category.findById(req.params.id).exec(callback);
+      },
+      category_items(callback) {
+        Item.find({ category: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.category == null) {
+        const error = new Error('Category not found');
+        error.status = 404;
+        return next(error);
+      }
+      res.render('category_details', {
+        title: results.category.title,
+        category: results.category,
+        category_items: results.category_items,
+      });
+    }
+  );
 };
 
 exports.category_delete_get = (req, res) => {
