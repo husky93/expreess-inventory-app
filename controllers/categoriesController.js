fb0c128b19ffe27db4ca0506696c4c4b75dacf65
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-03 12:11:16 +0100

    Delete category on delete request from client

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -72,8 +72,40 @@ exports.category_delete_get = (req, res, next) => {
   );
 };
 
-exports.category_delete_post = (req, res) => {
-  res.send('NOT IMPLEMENTED: Category Detail POST');
+exports.category_delete_post = (req, res, next) => {
+  async.parallel(
+    {
+      category(callback) {
+        Category.findById(req.params.id).exec(callback);
+      },
+      category_items(callback) {
+        Item.find({ category: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.category == null) {
+        const error = new Error('Category not found');
+        error.status = 404;
+        return next(error);
+      }
+      if (results.category_items.length > 0) {
+        res.render('category_delete', {
+          title: 'Delete Category',
+          category: results.category,
+          category_items: results.category_items,
+        });
+      }
+      Category.findByIdAndRemove(req.body.categoryid, (err) => {
+        if (err) {
+          return next(err);
+        }
+        res.redirect('/categories');
+      });
+    }
+  );
 };
 
 exports.category_create_get = (req, res) => {
